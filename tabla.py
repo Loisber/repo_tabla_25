5c45724b107a2c9b5be0e56a73cf47a2405834ea
luis adrian calvo madrigal <adriancalvo02@gmail.com>
2025-04-07 17:08:16 -0600

    metodo mostarInvertido terminado

--- tabla.py
+++ tabla.py
@@ -5,3 +5,8 @@ class Tabla:
     def mostrarNormal(self):
         for i in range(1,11):
             print(f"{self.numero}X{i}={self.numero * i}")
+
+    def mostarInvertido(self):
+        for i in range(10,0,-1):
+            print(f"{self.numero}X{i}={self.numero * i}")
+
